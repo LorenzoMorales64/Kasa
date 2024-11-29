4b0ec4fc3c261167a0e7ac8f872bb8506e8b05e9
LorenzoMorales <mrllorenzo@hotmail.com>
2024-11-29 20:54:55 +0100

    Création de la gallery

---
+++ src/page/gallery/Gallery.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import mainBanner from "../../assets/images/homeBanner.png";
+import "./Gallery.scss";
+import cards_data from "../../data/logement.json";
+import { Link } from "react-router-dom";
+
+export interface CardData {
+   id: string
+   title: string
+    cover: string
+    pictures: string[]
+    description:string
+    host:{
+        name:string
+        picture:string
+     }
+     rating:string
+     location:string
+     equipments:string[]
+     tags:string[]
+}
+
+export const Gallery: React.FC = () => {
+    return (
+        <div className="gallery">
+            <div className="mainBanner">
+                <img src={mainBanner} alt="Photo de mer" />
+                <h1>Chez vous, partout et ailleurs</h1>
+            </div>
+            <div className="cards-container">
+              {cards_data.map((cardData:CardData)=>(
+                <Link key={cardData.id} to={{pathname:`card/${cardData.id}`}}>
+                    <div className="card">
+                    <img src={cardData.cover} alt="Cover" />
+                    <h2>{cardData.title}</h2>
+                    </div>
+                </Link>
+              ))
+              }
+            </div>
+        </div>
+    )
+}
